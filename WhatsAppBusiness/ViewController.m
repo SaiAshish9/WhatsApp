365663c36e363ebce0c9a2fe9fd87b6392245983
saiashish9 <saiashish3760@gmail.com>
2022-04-18 01:21:46 +0530

    updated nav

--- WhatsAppBusiness/ViewController.m
+++ WhatsAppBusiness/ViewController.m
@@ -15,7 +15,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorNamed:  @"nav"];
+    self.view.backgroundColor = [UIColor colorNamed:  @"home"];
+    UIView *navbar = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width,self.view.frame.size.height * 0.17)];
+//    self.view.bound.size.width
+    navbar.backgroundColor = [UIColor colorNamed:  @"nav"];
+    [self.view addSubview:navbar];
+    
+//    UIView *fab = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width,self.view.frame.size.height * 0.17)];
+////    self.view.bound.size.width
+//    fab.backgroundColor = [UIColor colorNamed:  @"fab"];
+//    [self.view addSubview:fab];
+
 }
 
 
