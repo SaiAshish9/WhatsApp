80993754aad8b6288a0cb75016730407c8cc67d6
saiashish9 <saiashish3760@gmail.com>
2022-04-23 17:39:21 +0530

    custom fonts updated

--- WhatsAppBusiness/Screens/Home/Components/Fab/ViewController+FabController.m
+++ WhatsAppBusiness/Screens/Home/Components/Fab/ViewController+FabController.m
@@ -33,5 +33,4 @@
     NSLog(@"%@",NSStringFromCGPoint(location));
 }
 
-
 @end
--- WhatsAppBusiness/Screens/Home/Components/Nav/ViewController+HomeNavbar.m
+++ WhatsAppBusiness/Screens/Home/Components/Nav/ViewController+HomeNavbar.m
@@ -13,6 +13,15 @@
     UIView *navbar = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width,self.view.frame.size.height * 0.17)];
 //    self.view.bound.size.width
     navbar.backgroundColor = [UIColor colorNamed:  @"nav"];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(18, 27, self.view.frame.size.width*0.6, 90)];
+    label.backgroundColor = [UIColor clearColor];
+    label.textAlignment = NSTextAlignmentLeft;
+    label.textColor = [UIColor colorNamed:@"silver"];
+    label.numberOfLines = 1;
+    label.text = @"WhatsApp Business";
+//    sectionLabel.font = [UIFont fontWithName:@"TrebuchetMS-Bold" size:18];
+    label.font = [UIFont systemFontOfSize:18 weight: UIFontWeightSemibold];
+    [navbar addSubview:label];
     [self.view addSubview:navbar];
 }
 
