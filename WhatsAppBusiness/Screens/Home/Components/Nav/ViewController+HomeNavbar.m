01d2a40133c5f194b2b1c3bc75433bc9dd0b260f
saiashish9 <saiashish3760@gmail.com>
2022-04-24 14:18:39 +0530

    updated nav

--- WhatsAppBusiness/Screens/Home/Components/Nav/ViewController+HomeNavbar.m
+++ WhatsAppBusiness/Screens/Home/Components/Nav/ViewController+HomeNavbar.m
@@ -11,8 +11,19 @@
 
 - (void) addNavContainer {
     UIView *navbar = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width,self.view.frame.size.height * 0.17)];
-    //    self.view.bound.size.width
     navbar.backgroundColor = [UIColor colorNamed:  @"nav"];
+    
+    UILabel *label = [self addLabel];
+    UIImageView *searchIcon = [self addSearchIcon];
+    UIImageView *ellipsisIcon = [self addEllipsisIcon];
+
+    [navbar addSubview:label];
+    [navbar addSubview:searchIcon];
+    [navbar addSubview:ellipsisIcon];
+    [self.view addSubview:navbar];
+}
+
+-(UILabel*) addLabel {
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(18, 63, self.view.frame.size.width*0.6, 18)];
     label.backgroundColor = [UIColor clearColor];
     label.textAlignment = NSTextAlignmentLeft;
@@ -20,23 +31,27 @@
     label.numberOfLines = 1;
     label.text = @"WhatsApp Business";
     label.font = [UIFont fontWithName:@"Assistant-SemiBold" size:18];
+    return label;
     //    label.font = [UIFont systemFontOfSize:18 weight: UIFontWeightSemibold];
+    //    self.view.bound.size.width
     //  https://developer.apple.com/sf-symbols/
-    
-    UIImage *image1 = [UIImage systemImageNamed:@"magnifyingglass" ];
-    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:image1];
-    imageView1.tintColor = [UIColor colorNamed:  @"silver"];
-    imageView1.frame = CGRectMake(self.view.frame.size.width * 0.78 + 18, 62, imageView1.frame.size.width, imageView1.frame.size.height);
-    [navbar addSubview:imageView1];
-    
+}
+
+-(UIImageView*) addSearchIcon {
+    UIImage *image = [UIImage systemImageNamed:@"magnifyingglass" ];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.tintColor = [UIColor colorNamed:  @"silver"];
+    imageView.frame = CGRectMake(self.view.frame.size.width * 0.78 + 18, 62, imageView.frame.size.width, imageView.frame.size.height);
+    return imageView;
+}
+
+-(UIImageView*) addEllipsisIcon {
     UIImage *image = [UIImage systemImageNamed:@"ellipsis" ];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     imageView.tintColor = [UIColor colorNamed:  @"silver"];
     imageView.frame = CGRectMake(self.view.frame.size.width * 0.78 + 50, 68, imageView.frame.size.width, imageView.frame.size.height);
     imageView.transform = CGAffineTransformMakeRotation(-M_PI / 2);
-    [navbar addSubview:imageView];
-    [navbar addSubview:label];
-    [self.view addSubview:navbar];
+    return imageView;
 }
 
 @end
